efbc3fb0c18d4e3a8f760d80842c5dc8e449a86a
50strings <119496310+50strings@users.noreply.github.com>
2022-12-01 00:05:37 +0900

    Update calc.js
    罗莎莉亚伤害改为q

--- resources/meta/character/罗莎莉亚/calc.js
+++ resources/meta/character/罗莎莉亚/calc.js
@@ -9,6 +9,7 @@ export const details = [{
   dmg: ({ talent }, dmg) => dmg(talent.q['冰枪持续伤害'], 'q')
 }]
 
+export const defDmgIdx = 1
 export const mainAttr = 'atk,cpct,cdmg'
 
 export const buffs = [{
