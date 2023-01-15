988436cf3a4a7d0854d721035f35c47b0341fbab
tanyiok1234 <tanyiok1234@gmail.com>
2023-01-15 13:01:17 +0900

    bugfix

--- resources/meta/character/神里绫华/artis.js
+++ resources/meta/character/神里绫华/artis.js
@@ -2,7 +2,7 @@ export default function ({ attr, rule, def }) {
   if (attr.mastery > 120) {
     return rule('神里-精通', { atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, recharge: 30 })
   }
-  if (wn = '雾切之回光') {
+  if (attr.wn = '雾切之回光') {
     return rule('神里-雾切', { atk: 100, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, recharge: 30 })
   }
   return def({ atk: 75, cpct: 100, cdmg: 100, dmg: 100, phy: 100, recharge: 30 })
