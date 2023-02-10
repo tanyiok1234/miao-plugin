5b7c71d59ca64a65c0947611dfdd0db516fa71ae
SheenaU <119660605+SheenaU@users.noreply.github.com>
2023-02-10 16:29:39 +0800

    久岐忍超绽尝试修复bug

--- resources/meta/character/久岐忍/artis.js
+++ resources/meta/character/久岐忍/artis.js
@@ -1,6 +1,6 @@
 export default function ({ attr, rule, def }) {
-  if (attr.em > 160) {
+  if (attr.mastery > 160) {
     return rule(‘久岐忍-超绽’ , {hp: 100, atk: 50, def: 0, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, phy: 0, recharge: 55, heal: 100})
   }
-  return def({hp: 100, atk: 50, def: 0, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, phy: 0, recharge: 55, heal: 100 }）
+  return def({hp: 100, atk: 50, def: 0, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, phy: 0, recharge: 55, heal: 100 })
 }
